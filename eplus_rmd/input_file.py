6f98eb7725a7842dad4096c7ba285c71bbe587d9
Jason Glazer <jglazer@gard.com>
2022-09-22 14:19:24 -0500

    Split large input_file.py __init__ function into smaller pieces

--- eplus_rmd/input_file.py
+++ eplus_rmd/input_file.py
@@ -5,6 +5,11 @@ from pathlib import Path
 class InputFile:
 
     def __init__(self, epjson_file_path: Path):
+        self.load_epjson(epjson_file_path)
+        self.load_output_json(epjson_file_path)
+        self.load_hourly_output_json(epjson_file_path)
+
+    def load_epjson(self, epjson_file_path: Path):
         if not epjson_file_path.exists():
             raise Exception(f"Could not find input file at path: {epjson_file_path}")
         try:
@@ -14,6 +19,7 @@ class InputFile:
             print(f"Could not process input file into JSON object; error: {e}")
             raise
 
+    def load_output_json(self, epjson_file_path: Path):
         self.json_results_input_path = epjson_file_path.with_suffix(".json")
         if not self.json_results_input_path.exists():
             # try with the out.json suffix
@@ -28,6 +34,7 @@ class InputFile:
             print(f"Could not process results file into JSON object; error: {e}")
             raise
 
+    def load_hourly_output_json(self, epjson_file_path):
         self.json_hourly_results_input_path = epjson_file_path.with_name(epjson_file_path.stem + "_hourly.json")
         if not self.json_hourly_results_input_path.exists():
             self.json_hourly_results_input_path = epjson_file_path.with_name(epjson_file_path.stem + "out_hourly.json")
