5c097ba8ad1c7e6373badc69f29804534332fb66
Edwin Lee <leeed2001@gmail.com>
2024-04-30 14:32:16 -0500

    Update runner.py

--- energyplus_rmd/runner.py
+++ energyplus_rmd/runner.py
@@ -3,14 +3,18 @@ from sys import argv, exit
 from energyplus_rmd.translator import Translator
 
 
+def run_with_path(p: Path) -> int:
+    t = Translator(p)
+    t.process()
+    return 0
+
+
 def run() -> int:
     if len(argv) < 2:
         print("Need to pass at least 1 args: epJSON file")
         return 1
     epjson_input_file_path = Path(argv[1])
-    t = Translator(epjson_input_file_path)
-    t.process()
-    return 0
+    return run_with_path(epjson_input_file_path)
 
 
 if __name__ == "__main__":
