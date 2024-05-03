3ac2bd1f647a26c39584589671f54df9bafbf2fc
Jason Glazer <jglazer@gard.com>
2024-05-03 13:38:08 -0500

    Add run_example_files

---
+++ energyplus_rpd/scripts/run_examples.py
@@ -0,0 +1,15 @@
+from energyplus_rpd.runner import run_with_path
+from pathlib import Path
+
+
+def run_example_files():
+    print('before run of example files')
+    try:
+        run_with_path(Path('../example/ASHRAE901_OfficeSmall_STD2019_Denver.epJSON'))
+    except Exception as e:
+        print(f"Could not process example file; error: {e}")
+        raise
+    print('finished running example files')
+
+if __name__ == "__main__":
+    exit(run_example_files())
