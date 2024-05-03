3b68381c403b5af65c30ab22a791ace17eec2c58
Jason Glazer <jglazer@gard.com>
2024-05-03 15:39:34 -0500

    Fix the flake8 part

--- energyplus_rpd/runner.py
+++ energyplus_rpd/runner.py
@@ -2,10 +2,12 @@ from pathlib import Path
 from sys import argv, exit
 from energyplus_rpd.translator import Translator
 
+
 def run_with_path(p: Path) -> int:
     t = Translator(p)
     t.process()
     return 0
+
 
 def run() -> int:
     if len(argv) < 2:
