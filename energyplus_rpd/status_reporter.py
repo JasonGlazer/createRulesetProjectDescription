26a03ffe41289e40e36c4081e5f09b914d0a78f6
Jason Glazer <jglazer@gard.com>
2024-07-17 11:02:30 -0500

    Updated status reporter and schema notes related to status.

--- energyplus_rpd/status_reporter.py
+++ energyplus_rpd/status_reporter.py
@@ -26,6 +26,7 @@ class StatusReporter:
             # since the report is just for internal tracking of development
             # it fails gracefully when the file is not present which would only
             # be present for the developer.
+            still_to_do = []
             with open(self.report_file_path, 'w') as f:
                 print('============= Generated Report ==============', file=f)
                 print(f'Updated at: {datetime.now()} \n', file=f)
@@ -38,7 +39,7 @@ class StatusReporter:
                             counter = {'inout ': 0, 'input ': 0, 'output': 0, 'note  ': 0, 'null  ': 0}
                             status_count = {'DoneUsingInput': 0, 'DoneUsingOutput': 0, 'DoneUsingConstant': 0,
                                             'PartialUsingInput': 0, 'PartialUsingOutput': 0, 'PartialUsingConstant': 0,
-                                            'NotRequired': 0, 'NotStarted': 0, 'ToDo': 0}
+                                            'NotRequired': 0, 'NotStarted': 0, 'ToDo': 0, 'ComplianceParameter': 0}
                             print(f'  #elements: {len(data_elements)}', file=f)
                             for data_element in data_elements:
                                 fields = data_elements[data_element]
@@ -51,9 +52,19 @@ class StatusReporter:
                                     else:
                                         print(f'EPstatus of "{status}" is invalid in data element "{data_element}"'
                                               f' in data group "{data_group_name}"')
+                                    if status == 'ToDo':
+                                        still_to_do.append((data_group_name, data_element))
                                 print('  ' + _type + '  ' + status.ljust(25, ' ') + data_element, file=f)
                                 counter[_type] = counter[_type] + 1
-                            print(f'  counts:  {counter} {status_count} \n', file=f)
+                            print(f'  counts:  {counter}', file=f)
+                            for k, v in status_count.items():
+                                if v != 0:
+                                    print(f'     {k:<25}:  {v}', file=f)
+                            print('', file=f)
+                print('')
+                print('============== To Do ==============', file=f)
+                for item in still_to_do:
+                    print(item, file=f)
 
     @staticmethod
     def type_of_ep_field(fields):
