cf6401f0be674e8727eb3acd52f9dfb81962d1ad
Jason Glazer <jglazer@gard.com>
2025-04-22 15:12:52 -0500

    Flake8 to yaml_data_elements.py

--- utilities/yaml_data_elements.py
+++ utilities/yaml_data_elements.py
@@ -1,7 +1,7 @@
-## file that will read in the schema configuration given the path to the YAML schema (schema_yaml_path)
-## then looks at all json files in the folder given by folder_containing_jsons
-## outputs a file to output_yaml_path that gives a summary of all the data groups and data elements used
-## by the jsons in the folder_containing_jsons
+# file that will read in the schema configuration given the path to the YAML schema (schema_yaml_path)
+# then looks at all json files in the folder given by folder_containing_jsons
+# outputs a file to output_yaml_path that gives a summary of all the data groups and data elements used
+# by the jsons in the folder_containing_jsons
 
 import json
 import yaml
@@ -9,7 +9,7 @@ import os
 from pathlib import Path
 
 folder_containing_jsons = os.path.join(
-    Path(__file__).parents[1],"ruletest_engine","ruletest_jsons","ashrae9012019"
+    Path(__file__).parents[1], "ruletest_engine", "ruletest_jsons", "ashrae9012019"
 )
 
 print("folder containing jsons: " + folder_containing_jsons)
@@ -22,14 +22,15 @@ output_yaml_path = os.path.join(
     Path(__file__).parents[0], "data_elements_used.yaml"
 )
 
-def used_keys(object_hash,found_keys,data_group_name,reference_hash):
+
+def used_keys(object_hash, found_keys, data_group_name, reference_hash):
     found_objects = []
     found_keys[data_group_name]["exists"] = True
     for key in object_hash:
         if key in found_keys[data_group_name]:
             found_keys[data_group_name][key] = True
         if key in reference_hash[data_group_name]:
-            if isinstance(object_hash[key],list):
+            if isinstance(object_hash[key], list):
                 for h in object_hash[key]:
                     found_objects.append({reference_hash[data_group_name][key]: h})
             else:
@@ -37,24 +38,24 @@ def used_keys(object_hash,found_keys,data_group_name,reference_hash):
     for new_object in found_objects:
         for new_data_group_name in new_object:
             new_object_hash = new_object[new_data_group_name]
-            used_keys(new_object_hash,found_keys,new_data_group_name,reference_hash)
+            used_keys(new_object_hash, found_keys, new_data_group_name, reference_hash)
 
 
 with open(schema_yaml_path) as f:
-    schema_yaml_dict = yaml.load(f,Loader = yaml.Loader)
+    schema_yaml_dict = yaml.load(f, Loader=yaml.Loader)
 
-### for the yaml schema parsing:
+# for the yaml schema parsing:
 yaml_found_keys = {}
 yaml_reverse_data_groups = {}
 
-## fill the data_groups dictionary with keys before doing next step
+# fill the data_groups dictionary with keys before doing next step
 for data_group_name in schema_yaml_dict:
     if schema_yaml_dict[data_group_name]["Object Type"] == "Data Group":
         yaml_found_keys[data_group_name] = {}
         yaml_found_keys[data_group_name]["exists"] = False
         yaml_reverse_data_groups[data_group_name] = {}
 
-## now look for where these data groups are referenced by other data groups
+# now look for where these data groups are referenced by other data groups
 for data_group_name in schema_yaml_dict:
     print("data group name: " + data_group_name)
     if schema_yaml_dict[data_group_name]["Object Type"] == "Data Group":
@@ -63,7 +64,8 @@ for data_group_name in schema_yaml_dict:
             yaml_found_keys[data_group_name][data_group_element] = False
 
             if "Data Type" in schema_yaml_dict[data_group_name]["Data Elements"][data_group_element]:
-                # didn't check for # Data Type: "(<OutputSchemaOptions2019ASHRAE901>, <OutputSchemaOptions2019T24>, OutputSchemaOptionsRESNET)"
+                # didn't check for # Data Type:
+                # "(<OutputSchemaOptions2019ASHRAE901>, <OutputSchemaOptions2019T24>, OutputSchemaOptionsRESNET)"
                 # "{}"
 
                 data_type = schema_yaml_dict[data_group_name]["Data Elements"][data_group_element]["Data Type"]
@@ -72,12 +74,13 @@ for data_group_name in schema_yaml_dict:
                 # "[{}]"
                 elif "[{" == data_type[:2]:
                     ref = data_type[2:(len(data_type)-2)]
-                else: ref = ""
+                else:
+                    ref = ""
                 if ref in yaml_reverse_data_groups:
                     yaml_reverse_data_groups[data_group_name][data_group_element] = ref
-                    #print("ref " + data_group_name + ": " + data_group_element + "; " + ref)
+                    # print("ref " + data_group_name + ": " + data_group_element + "; " + ref)
 
-subfolders = [ f.path for f in os.scandir(folder_containing_jsons) if f.is_dir() ]
+subfolders = [f.path for f in os.scandir(folder_containing_jsons) if f.is_dir()]
 for subfolder in subfolders:
     for root, dirs, files in os.walk(subfolder):
         for file in files:
@@ -88,8 +91,10 @@ for subfolder in subfolders:
                 for test_name in test_list_dictionary:
                     if "rmd_transformations" in test_list_dictionary[test_name]:
                         for transformation in test_list_dictionary[test_name]["rmd_transformations"]:
-                            used_keys(test_list_dictionary[test_name]["rmd_transformations"][transformation], yaml_found_keys,
-                                      "RulesetProjectDescription", yaml_reverse_data_groups)
+                            used_keys(test_list_dictionary[test_name]["rmd_transformations"][transformation],
+                                      yaml_found_keys,
+                                      "RulesetProjectDescription",
+                                      yaml_reverse_data_groups)
 
 
 yaml_output = yaml.dump(yaml_found_keys)
